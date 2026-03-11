4e3f5d71c04ce04a7d1d63a413a5e7ee192d6748
Kartik Garg <gargkartikxx@gmail.com>
2026-03-11 03:05:33 -0400

    fixed episode lenght and relaxted ee terminating condition

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/flat_env_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/flat_env_cfg.py
@@ -132,7 +132,8 @@ class G1FlatDeltaAOpenLoopEnvCfg(G1FlatEnvCfg):
         self.actions.joint_pos.scale = G1_ACTION_SCALE
 
         self.rewards.penalty_minimal_action_norm = RewTerm(func=mdp.penalty_minimal_action_norm, weight=-0.1)
-        self.episode_length_s = 1.0
+        self.terminations.ee_body_pos.params["threshold"] = 0.35
+        self.episode_length_s = 20.0
 
 
 @configclass
