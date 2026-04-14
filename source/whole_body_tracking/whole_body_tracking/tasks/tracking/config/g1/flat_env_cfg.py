5968bb93722c15ebc441ce19e2d65681cdf5e9be
Kartik Garg <gargkartikxx@gmail.com>
2026-04-14 17:18:41 -0400

    made body name switching easier

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/flat_env_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/flat_env_cfg.py
@@ -179,7 +179,38 @@ class G1FlatDeltaAOpenLoopEnvCfg(G1FlatEnvCfg):
             require_motion_action=True,
         )
         self.actions.joint_pos.scale = G1_ACTION_SCALE
-
+        self.commands.motion.body_names = [
+            "pelvis",
+            "left_hip_pitch_link",
+            "left_hip_roll_link",
+            "left_hip_yaw_link",
+            "left_knee_link",
+            "left_ankle_pitch_link",
+            "left_ankle_roll_link",
+            "right_hip_pitch_link",
+            "right_hip_roll_link",
+            "right_hip_yaw_link",
+            "right_knee_link",
+            "right_ankle_pitch_link",
+            "right_ankle_roll_link",
+            "waist_yaw_link",
+            "waist_roll_link",
+            "torso_link",
+            "left_shoulder_pitch_link",
+            "left_shoulder_roll_link",
+            "left_shoulder_yaw_link",
+            "left_elbow_link",
+            "left_wrist_roll_link",
+            "left_wrist_pitch_link",
+            "left_wrist_yaw_link",
+            "right_shoulder_pitch_link",
+            "right_shoulder_roll_link",
+            "right_shoulder_yaw_link",
+            "right_elbow_link",
+            "right_wrist_roll_link",
+            "right_wrist_pitch_link",
+            "right_wrist_yaw_link",
+        ]
         # Use global body pose rewards instead of relative-body pose rewards.
         self.rewards.motion_body_pos = None
         self.rewards.motion_body_ori = None
